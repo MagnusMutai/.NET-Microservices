e89795814694912814c6fdfec22e095d661d0d46
Magnus Mutai <MagnusMq07@gmail.com>
2024-09-16 11:59:07 +0300

    Creating the PlatformsController

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,13 +1,27 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
+using PlatformService.Dtos;
 
 namespace PlatformService.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class PlatformsController : ControllerBase
 {
+    private readonly IPlatformRepo repository;
+    private readonly IMapper mapper;
+
     public PlatformsController(IPlatformRepo repository, IMapper mapper)
     {
-        
+        this.repository = repository;
+        this.mapper = mapper;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms...");
+    }
+
 }
